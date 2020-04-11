d8711a31b0bfbe3c072a04e6412a29be864926ad
ubuntu <ubuntu@localhost.localdomain>
2020-04-11 13:51:20 +0800

    add say hello

---
+++ flask_demo.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/user/<name>')
+def index(name):
+    return '''<h1>Hello {}</h1>'''.format(name)
+
+
+if __name__ == '__main__':
+    app.run(port=5010)
